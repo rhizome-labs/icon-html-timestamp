eec807a597e33bf54af9bb93faccb28d618c78d7
Brian Li <brian@decrypto.net>
2019-08-14 17:50:55 +0900

    Update icon-html-timestamping-tool.py

--- icon-html-timestamping-tool.py
+++ icon-html-timestamping-tool.py
@@ -2,24 +2,32 @@ import datetime
 import iconsdk
 import random
 import requests
-import sys
 from iconsdk.wallet.wallet import KeyWallet
 from iconsdk.builder.transaction_builder import (TransactionBuilder, DeployTransactionBuilder, CallTransactionBuilder, MessageTransactionBuilder)
 from iconsdk.signed_transaction import SignedTransaction
 from iconsdk.icon_service import IconService
 from iconsdk.providers.http_provider import HTTPProvider
 
-#Get URL info
+#Get URL header
 url = input("What URL would you like to timestamp? (e.g. https://rhizomeicx.com) ")
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 response = requests.get(url)
 
-if response.status_code == 404:
-   	url = input("404 error! Please provide a valid URL. ")
+#Filter through common HTTP error codes
+if response.status_code == 401:
+   	url = input("401 error! Please provide a valid URL. ")
+elif response.status_code == 402:
+	url = input("403 error! Please provide a valid URL. ")
 elif response.status_code == 403:
 	url = input("403 error! Please provide a valid URL. ")
+elif response.status_code == 404:
+	url = input("404 error! Please provide a valid URL. ")
+elif response.status_code == 500:
+	url = input("500 error! Please provide a valid URL. ")
 elif response.status_code == 502:
 	url = input("502 error! Please provide a valid URL. ")
+elif response.status_code == 502:
+	url = input("503 error! Please provide a valid URL. ")
 elif response.status_code == 504:
 	url = input("504 error! Please provide a valid URL. ")
 
@@ -36,32 +44,32 @@ utc_datetime = date.strftime("%Y-%m-%d %H:%M:%S")
 #Scrape URL
 page = requests.get(url, headers=headers)
 html = page.content.decode()
-info = "This timestamp of " + str(url) + " was taken at " + str(utc_datetime) + " UTC." + "\n\n" 
-html_hex = html.encode("utf-8").hex()
-info_hex = info.encode("utf-8").hex()
-tx_message = info_hex + html_hex
-tx_message_length = len(tx_message)
 
-#Error Codes
+#Prepare ICX transaction message
+html_hex = html.encode("utf-8").hex()
+header = "This timestamp of " + str(url) + " was taken at " + str(utc_datetime) + " UTC." + "\n\n"
+header_hex = header.encode("utf-8").hex()
+footer = "\n\nTimestamped with ProofOfRecord v0.1 by RHIZOME - https://bit.ly/2Z4Dd8C"
+footer_hex = footer.encode("utf-8").hex()
+tx_message = header_hex + html_hex + footer_hex
+tx_message_length = len(tx_message)
 
 #Create ICX transaction
 if tx_message_length > 509950:
 	print("This HTML page is too large to timestamp in an ICX transaction.")
 elif tx_message_length <= 509950:
-	#print("This is a valid ICX transaction!")
 	wallet = KeyWallet.load(keystore_location_str, keystore_password_str)
-	#Build ICX transaction
 	transaction = MessageTransactionBuilder()\
-    		.from_(wallet.get_address())\
-    		.to("hxd4ed86cb5dafb842701683b8c35b21d28df563c3")\
-   		 .step_limit(100000000)\
-    		.data("0x" + tx_message)\
-    		.nid(1)\
-    		.nonce(random.randint(1,9999))\
-    		.build()
+		.from_(wallet.get_address())\
+		.to("hxd4ed86cb5dafb842701683b8c35b21d28df563c3")\
+		.step_limit(100000000)\
+		.data("0x" + tx_message)\
+		.nid(1)\
+		.nonce(random.randint(1,999))\
+		.build()
 	icon_service = IconService(HTTPProvider("https://wallet.icon.foundation/api/v3"))
 	signed_transaction = SignedTransaction(transaction, wallet)
 	tx_hash = icon_service.send_transaction(signed_transaction)
 
 #Print ICX transaction hash
-print("Tranaction Hash: " + tx_hash)
+print("\nSuccess! View your timestamped HTML at the URL below.\nhttps://tracker.icon.foundation/transaction/" + tx_hash + "\n")
