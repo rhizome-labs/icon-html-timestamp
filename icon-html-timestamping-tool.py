d3b876a0daf26a0bb8a460853ab8f64270813c2d
Brian Li <brian@decrypto.net>
2019-08-14 18:29:34 +0900

    Update icon-html-timestamping-tool.py

--- icon-html-timestamping-tool.py
+++ icon-html-timestamping-tool.py
@@ -1,3 +1,5 @@
+#RHIZOME ProofOfRecord v0.1 by @DecryptoBL
+import click
 import datetime
 import iconsdk
 import requests
@@ -30,12 +32,6 @@ elif response.status_code == 502:
 elif response.status_code == 504:
 	url = input("504 error! Please provide a valid URL. ")
 
-#Get ICONex wallet variables
-keystore_location = input("What is the file path of your keystore file? ")
-keystore_password = input("What is your keystore password? ")
-keystore_location_str = keystore_location.rstrip()
-keystore_password_str = keystore_password.rstrip()
-
 #Create data and time formatting
 date = datetime.datetime.utcnow()
 utc_datetime = date.strftime("%Y-%m-%d %H:%M:%S")
@@ -50,24 +46,36 @@ header = "This timestamp of " + str(url) + " was taken at " + str(utc_datetime) 
 header_hex = header.encode("utf-8").hex()
 footer = "\n\nTimestamped with ProofOfRecord v0.1 by RHIZOME - https://bit.ly/2Z4Dd8C"
 footer_hex = footer.encode("utf-8").hex()
-tx_message = header_hex + html_hex + footer_hex
+
+#Check if user would like to credit RHIZOME below timestamped HTML
+if click.confirm("Would you like to credit RHIZOME below your timestamped HTML?", default=True):
+    tx_message = header_hex + html_hex + footer_hex
+else:
+	tx_message = header_hex + html_hex
+
+#Calculate transaction message length
 tx_message_length = len(tx_message)
+if tx_message_length > 509950:
+	print("Sorry, this HTML page is too large to timestamp in an ICX transaction.")
+	exit()
+elif tx_message_length <= 509950:
+	keystore_location = input("What is the file path of your keystore file? ")
+	keystore_password = input("What is your keystore password? ")
+	keystore_location_str = keystore_location.rstrip()
+	keystore_password_str = keystore_password.rstrip()
 
 #Create ICX transaction
-if tx_message_length > 509950:
-	print("This HTML page is too large to timestamp in an ICX transaction.")
-elif tx_message_length <= 509950:
-	wallet = KeyWallet.load(keystore_location_str, keystore_password_str)
-	transaction = MessageTransactionBuilder()\
-		.from_(wallet.get_address())\
-		.to("hxd4ed86cb5dafb842701683b8c35b21d28df563c3")\
-		.step_limit(100000000)\
-		.data("0x" + tx_message)\
-		.nid(1)\
-		.build()
-	icon_service = IconService(HTTPProvider("https://wallet.icon.foundation/api/v3"))
-	signed_transaction = SignedTransaction(transaction, wallet)
-	tx_hash = icon_service.send_transaction(signed_transaction)
+wallet = KeyWallet.load(keystore_location_str, keystore_password_str)
+transaction = MessageTransactionBuilder()\
+	.from_(wallet.get_address())\
+	.to("hxd4ed86cb5dafb842701683b8c35b21d28df563c3")\
+	.step_limit(100000000)\
+	.data("0x" + tx_message)\
+	.nid(1)\
+	.build()
+icon_service = IconService(HTTPProvider("https://wallet.icon.foundation/api/v3"))
+signed_transaction = SignedTransaction(transaction, wallet)
+tx_hash = icon_service.send_transaction(signed_transaction)
 
-#Print ICX transaction hash
+#Print success message with link to transaction on ICON tracker
 print("\nSuccess! View your timestamped HTML at the URL below.\nhttps://tracker.icon.foundation/transaction/" + tx_hash + "\n")
